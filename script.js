b85678c19787473bf5c85879c74ff42eaccb8543
Cailin Young <cailinfayyoung@gmail.com>
2021-02-11 00:47:31 -0700

    finish javascript page :)

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 // moment api date display  
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-// 
-$(document).ready(function () {
-    $(".saveBtn").on("click", function () {
-        var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, value);
-    });
+// save button semantics
+$(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var value = $(this).siblings(".description").val();
+    localStorage.setItem(time, value);
+});
 
+//  local save data
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+$(document).ready(function () {
     function hourConfiguration() {
         var currentHour = moment().hours();
 
         $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+            const hourSlot = parseInt($(this).attr("id").split("-")[1]);
 
-            // check if we've moved past this time
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-            }
-            else if (blockHour === currentHour) {
+            if (hourSlot === currentHour) {
                 $(this).removeClass("past");
-                $(this).addClass("current");
+                $(this).addClass("future");
             }
+            else if (hourSlot < currentHour) {
+                    $(this).addClass("past");
+            }
+            
             else {
                 $(this).removeClass("past");
                 $(this).removeClass("current");
                 $(this).addClass("future");
             }
         });
-    }
+    }    
 
     hourConfiguration();
 
-    // save data 
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
 });
-
