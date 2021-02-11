a0f32fc4eac6462a52b484c61123ad021c2205af
Cailin Young <cailinfayyoung@gmail.com>
2021-02-10 19:30:41 -0700

    Adding java file

---
+++ script.js
@@ -0,0 +1,48 @@
+// moment api date display  
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+// 
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+    });
+
+    function hourConfiguration() {
+        var currentHour = moment().hours();
+
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+            // check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("current");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("current");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    hourConfiguration();
+
+    // save data 
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+});
+
